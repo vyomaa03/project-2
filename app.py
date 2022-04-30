629e276d0f7041cc878f5eb27daa1bb82a9bd366
Vyomaa Bhaskar <vyomaa03@gmail.com>
2022-04-30 17:23:21 +1000

    base and login pages for html and python app

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
+
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def home():
+    return render_template('base.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
