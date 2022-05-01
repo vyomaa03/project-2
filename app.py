7dc29b67bdaffd4e143c3869e8bcfd7daccd6396
Vyomaa Bhaskar <vyomaa03@gmail.com>
2022-05-01 16:27:24 +1000

    Created seperate html and css pages for login and signup and home page

--- app.py
+++ app.py
@@ -12,9 +12,13 @@ def home():
     return render_template('base.html')
 
 @app.route('/login')
-def login():
+def login(): # when the login button is pressed it should return this html
     return render_template('login.html')
 
+
+@app.route('/signup')
+def signup(): # when the login button is pressed it should return this html
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
