56521a6675758ae8325db398dc4f83d598eb3095
vyomaa03 <vyomaa03@gmail.com>
2022-05-03 07:59:27 +1000

    sql files edited with table information

---
+++ seed.sql
@@ -0,0 +1,6 @@
+/* using this file to insert all the elements and values in the table */
+/* table userinfo: columns: id, username,password?, home airport */
+
+
+INSERT INTO userinfo (username, user_password, home_airport) VALUES ('vyomaa03', abcdefg@, 'YGAW');
+INSERT INTO userinfo (username, user_password, home_airport) VALUES ('inaya08', abcdefgh@, 'YPAD');
