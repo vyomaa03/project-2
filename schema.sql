47a266fb96fe1446de46224fbb6c656d5a2493ad
vyomaa03 <vyomaa03@gmail.com>
2022-05-03 12:32:06 +1000

    sql edits

--- schema.sql
+++ schema.sql
@@ -1,10 +1,10 @@
 /* creating the table "drop table"  table: userinfo */
 
-DROP TABLE IF EXISTS userinfo;
+DROP TABLE IF EXISTS userinfo
 /*  Should I store passwords in my database to? 
     When I type in password I get in blue text so like is it a type of input as well?  
     If i can use it then how do i do it?*/
-CREATE TABLE userinfo (
+CREATE TABLE userinfo(
     id SERIAL PRIMARY KEY,
     username TEXT,
     user_password password,
--- seed.sql
+++ seed.sql
@@ -1,5 +1,5 @@
 /* using this file to insert all the elements and values in the table */
-/* table userinfo: columns: id, username,password?, home airport */
+/* table userinfo: columns: id, username,password?, home airport ahhhhhh this file wont connect to anything >.< */
 
 
 INSERT INTO userinfo (username, user_password, home_airport) VALUES ('vyomaa03', abcdefg@, 'YGAW');
